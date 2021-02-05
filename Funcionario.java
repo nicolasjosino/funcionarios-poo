15ea9cbfa36de4ab6a9aab3bbcaa3dca6b8fe364
Nicolas Josino <nicolasjosino@gmail.com>
2021-02-05 15:42:04 -0300

    Inclusão da classe Funcionario

---
+++ Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+  String nome;
+  String matricula;
+  boolean graduado;
+
+  public Funcionario(String nome, String matricula, boolean graduado){
+    this.nome = nome;
+    this.matricula = matricula;
+    this.graduado = graduado;
+  }
+}
